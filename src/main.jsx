6510eaec35dde39bf984b0c46145b9d64a4c7417
humberto <humbertolima108@gmail.com>
2024-03-15 04:45:43 -0300

    add design configs

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ConfigProvider } from 'antd'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ConfigProvider } from "antd";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ConfigProvider direction='ltr' theme={{
-    token:{
-      colorPrimary: "#158285"
-    }
-  }}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ConfigProvider
+    direction="ltr"
+    theme={{
+      token: {
+        colorPrimary: "#158285",
+      },
+      components: {
+        Table: {
+          headerBg: "#158285",
+          headerBorderRadius: 30,
+          headerSplitColor: "black",
+        },
+        Pagination: {
+          position: "center",
+        },
+      },
+    }}
+  >
     <App />
-  </ConfigProvider>,
-)
+  </ConfigProvider>
+);
