0cf4576f42eb609a55a038712513a6b48120938f
humberto <humbertolima108@gmail.com>
2024-03-14 21:52:00 -0300

    add search-list

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ConfigProvider } from 'antd'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ConfigProvider direction='ltr' theme={{
+    token:{
+      colorPrimary: "#158285"
+    }
+  }}>
     <App />
-  </React.StrictMode>,
+  </ConfigProvider>,
 )
